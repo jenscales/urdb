d72505b217f5611efbb63cf7dc1c5b103c81bf70
jenscales <jen_scales@yahoo.com>
2013-12-11 15:57:35 -0600

    add Cast star character name to show page

--- app/models/star.rb
+++ app/models/star.rb
@@ -1,3 +1,4 @@
 class Star < ActiveRecord::Base
-  
+  has_many :roles
+  has_many :movies, :through => :roles
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Urdb::Application.routes.draw do
   resources :movies do
     resources :showtimes
+
   end
+
+  resources :stars, only: [:show]
 
   root 'movies#index'
 end
